0297a18782b5e0c729c6ed6d2c7485485d83f36f
anon <dicktated@protonmail.com>
2021-10-13 13:19:52 +0200

    sacabo lo de hoy

--- main_app/views.py
+++ main_app/views.py
@@ -10,12 +10,6 @@ def index(request):
 def timeline(request):
     response = "Todos los Posts" 
     tl = Post.objects.order_by('-time')[:5]
-    #print(tl)
-    #op = '<li>'.join([p.text for p in tl])
     template = loader.get_template('timeline/tl.html')
     context = { 'tl': tl }
-    print(HttpResponse(template.render(context, request)))
     return HttpResponse(template.render(context, request))
-    # return render(request, 'timeline/tl.html', context)
-    # return HttpResponse(response),
-
